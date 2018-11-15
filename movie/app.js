49568883bf65ab252c99e6f2c4a04d7fcef48a2b
msminsu <msminsu@gmail.com>
2018-11-15 22:15:43 +0900

    수정

--- movie/app.js
+++ movie/app.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
 
         function mainVid(id){
             $('#video').html(`
-            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/${id}?autoplay=1&amp;loop=1&amp;playsinline=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/${id}?playsinline=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             `);
         }
 
