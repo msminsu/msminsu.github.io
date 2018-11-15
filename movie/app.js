a46d0af094a92f1df708e6a1c6f3613eb6f1328c
An-gun <msminsu1@ncsoft.com>
2018-11-15 14:43:34 +0900

    change

--- movie/app.js
+++ movie/app.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
 
 
     var key = "AIzaSyB90nzT-0F7xbOR0NpIU1N5HVhDJy0zEic";
+    // var playlistId = "PLJUYgaCc1xMHn9w-KQ8S9kieA3vm8Vtt_"
     var playlistId = "PL5ZDxMFT4aldNMe9An83jJJH4GOXIrt2X"
     var URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
 
         var options ={
             part: 'snippet',
             key: key,
-            maxResults: 20,
+            maxResults: 50,
             playlistId: playlistId,
-
         }
 
         loadVids();
@@ -18,7 +18,7 @@ $(document).ready(function(){
 
         function  loadVids(){
             $.getJSON(URL, options, function(data){
-                console.log(data);
+                // console.log(data);
                 var id = data.items[0].snippet.resourceId.videoId;
                 mainVid(id);
                 resultsLoop(data); 
@@ -34,16 +34,11 @@ $(document).ready(function(){
         }
 
         function resultsLoop(data){
-
-            
-
-
             $.each(data.items, function(i,item){
                 var thumb = item.snippet.thumbnails.medium.url;
                 var title = item.snippet.title;
                 var desc = item.snippet.description.substring(0, 20);
                 var vid = item.snippet.resourceId.videoId;
-
 
 
 
@@ -55,7 +50,25 @@ $(document).ready(function(){
                     </div>
                 </article>
                 `);
+            });
+        }
 
+        function resultsLoop2(data){
+            $('main').html('');
+            console.log(data);
+            $.each(data.items, function(i,item){
+                var thumb = item.snippet.thumbnails.medium.url;
+                var title = item.snippet.title;
+                var desc = item.snippet.description.substring(0, 20);
+                var pid = item.id.videoId;
+                $('main').append(`
+                <article class="item" data-key="${pid}">
+                    <img src="${thumb}" alt=""  class="thumb">
+                    <div class="details">
+                        <h4>${title}</h4>
+                    </div>
+                </article>
+                `);
             });
         }
 
@@ -63,6 +76,39 @@ $(document).ready(function(){
             var id = $(this).attr('data-key');
             mainVid(id);
         });
+        
+        $('input').on('change',function(e){
+            // console.log(e.target.value, 'check)');
+            var opt={
+                part: 'snippet',
+                key: key,
+                maxResults: 20,
+                q:e.target.value
+            }
+            
+
+            // https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&q=${val || defaultKeyword}&part=snippet&type=video&maxResults=${max}
+            search(`https://www.googleapis.com/youtube/v3/search?part=snippet
+            &q=${e.target.value}
+            &type=video
+            &key=${key}`,opt);
+            /* var id = $(this).attr('data-key');
+            mainVid(id); */
+        });
+
+
+
+        
+          function search(URL,opt) {
+            $.getJSON(URL, opt, function(data){
+                console.log(data);
+                
+                // var id = data.items[0].snippet.resourceId.videoId;
+                // mainVid(id);
+                resultsLoop2(data); 
+                
+            })
+          }
         
 
 });
