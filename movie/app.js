0b25d2f838f753d37da26c4e7428097a447e780b
msminsu <msminsu@gmail.com>
2018-11-16 10:31:33 +0900

    add

--- movie/app.js
+++ movie/app.js
@@ -32,10 +32,12 @@ $(document).ready(function(){
             <iframe id="ifr" width="100%" height="100%" style="box-sizing: border-box;position:absolute; top:0;left:0;right:0;bottom:0;"
             sandbox="allow-modals allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts"
             frameborder="0"
+            sandbox="allow-scripts allow-same-origin allow-presentation"
             src="video.html"
             >
             </iframe>
             `);
+            sendChildMessage(id);
         }
 
         // <iframe width="100%" height="100%" src="https://www.youtube.com/embed/${id}?playsinline=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
@@ -85,7 +87,9 @@ $(document).ready(function(){
 
         $('main').on('click','article',function(){
             var id = $(this).attr('data-key');
-            mainVid(id);
+           // mainVid(id);
+           // 자식 iframe 에 전달
+           sendChildMessage(id);
         });
         
         $('input').on('change',function(e){
@@ -97,7 +101,6 @@ $(document).ready(function(){
                 q:e.target.value
             }
             
-
             // https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&q=${val || defaultKeyword}&part=snippet&type=video&maxResults=${max}
             search(`https://www.googleapis.com/youtube/v3/search?part=snippet
             &q=${e.target.value}
@@ -113,44 +116,28 @@ $(document).ready(function(){
           function search(URL,opt) {
             $.getJSON(URL, opt, function(data){
                 console.log(data);
-                
                 // var id = data.items[0].snippet.resourceId.videoId;
                 // mainVid(id);
                 resultsLoop2(data); 
-                
             })
           }
 
 
-      
-    //    window.addEventListener("message", processFn, false);		
+    // 자식 iframe 이벤트 등록
+       window.addEventListener("message", processFn, false);		
     
-    $(document).on('click','main .btn',function(){
-        sendChildMessage();
-        console.log('chekc')
-    });
-
-
-    
+    // 자식 iframe 이벤트 적용
         function processFn(event) {
-    
             var bla = event.data;
-    
-            console.log('parent',bla);
-    
+            console.log('parent',bla);    
         }
     
         
     
-        function sendChildMessage() {	
-
-            console.log('sendCh');
-
-    var opt = {
-        a: 10,
-        b: 20
-    }
-            document.getElementById("ifr").contentWindow.postMessage(opt, '*');
+        // 자식 iframe 으로 인자 전달
+        function sendChildMessage(id) {	
+            // console.log('sendCh');
+            document.getElementById("ifr").contentWindow.postMessage(id, '*');
     
         }	
         
