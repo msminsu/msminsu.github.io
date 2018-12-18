848eed1168055a38392b5d185a6705d216ad7ea5
An-gun <msminsu1@ncsoft.com>
2018-12-18 09:08:23 +0900

    loop

--- youtube/app.js
+++ youtube/app.js
@@ -2,13 +2,14 @@ $(document).ready(function(){
 
 
     var key = "AIzaSyB90nzT-0F7xbOR0NpIU1N5HVhDJy0zEic";
-    var playlistId = "PLu8EoSxDXHP6CGK4YVJhL_VWetA865GOH"
+    // var playlistId = "PLu8EoSxDXHP6CGK4YVJhL_VWetA865GOH"  //javascript 30
+    var playlistId = "PL5ZDxMFT4aldNMe9An83jJJH4GOXIrt2X"  //라푼젤
     var URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
 
         var options ={
             part: 'snippet',
             key: key,
-            maxResults: 30,
+            maxResults: 50,
             playlistId: playlistId,
 
         }
@@ -28,14 +29,19 @@ $(document).ready(function(){
 
         function mainVid(id){
             $('#video').html(`
-            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/${id}?autoplay=1&loop=1&cc_load_policy=1rel=0&amp;showinfo=0&playlist=${id}" frameborder="0" allowfullscreen></iframe>
+            
             `);
+            //<iframe width="100%" height="100%" src="https://www.youtube.com/embed/${id}?autoplay=1&loop=1;" frameborder="0" allow="accelerometer; autoplay; loop; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            //<iframe width="100%" height="100%" src="https://www.youtube.com/embed//${id}?autoplay=1&loop=1&cc_load_policy=1rel=0&amp;controls=0&amp;showinfo=0&playlist=Yo19ZhO7CAc" frameborder="0" allowfullscreen></iframe>
         }
 
         function resultsLoop(data){
 
             
             $('main').html('');
+
+            console.log(data.items);
 
             $.each(data.items, function(i,item){
                 var thumb = item.snippet.thumbnails.medium.url;
